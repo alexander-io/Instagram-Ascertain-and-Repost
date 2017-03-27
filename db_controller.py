7ef2585751f4934f811fce8c68012fb20fc351f0
Alexander Harris <abharris@pugetsound.edu>
2017-03-27 15:18:45 -0700

    ++comments

--- db_controller.py
+++ db_controller.py
@@ -1,17 +1,13 @@
+# controller function that's used to make a new entry in db
 import pymongo
-# from pymongo.objectid import ObjectId
 from bson.objectid import ObjectId
 import pprint
 
-
-
-
+# here's a function that's used to post to the database and make a new entry
 def post_to_base(post):
     client = pymongo.MongoClient()
     db = client.post_database
     posts = db.posts
 
-    # post = {'key':'val'}
-    
     post_id = posts.insert_one(post).inserted_id
     return post_id
