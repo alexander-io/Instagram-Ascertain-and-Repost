631f22547a3a5a321187ffe7880c8554cf792bd8
Alexander Harris <abharris@pugetsound.edu>
2017-03-27 22:27:12 -0700

    update drop_db script, comments++

--- drop_db.py
+++ drop_db.py
@@ -2,9 +2,8 @@
 # from the database, this script is executed on issuance of $ make clean
 
 import pymongo
+
 client = pymongo.MongoClient()
-
 db = client.post_database
-
 result = db.posts.delete_many({})
 print('db items deleted : ', result.deleted_count)
