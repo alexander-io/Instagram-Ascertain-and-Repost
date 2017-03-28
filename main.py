f61aabe2ddda9e0950a22b748d445215714e0494
Alexander Harris <abharris@pugetsound.edu>
2017-03-27 22:25:31 -0700

    add post_map file line removal for each dequeue() of queue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main method
-
+import copy
 import x
 # from Instagram-API-python/InstagramAPI import InstagramAPI
 import pymongo
@@ -9,35 +9,12 @@ import os
 import sys
 from bson.objectid import ObjectId
 import pprint
-# import imp
-
 sys.path.append('Instagram-API-python')
-# sys.path[0:0] = './Instagram-API-python'
-
-# InstagramAPI = imp.load_source('InstagramAPI', 'Instagram-API-python/InstagramAPI.py')
 from InstagramAPI import InstagramAPI
 
-# configfile = 'Instagram-API-python/InstagramAPI.py'
-# sys.path.append(os.path.expanduser(configfile))
-# from InstagramAPI import InstagramAPI
-# import InstagramAPI
 
-
-
-
-
-
-
+# main routine for instagram ascertain & repost
 def main():
-    # declare & fill the queue with ObjectId types, corresponding to the values contained in the post_map
-    q = queue.Queue()
-    q = fill_queue(q)
-
-    # start the mongo client, access the posts database
-    client = pymongo.MongoClient()
-    db = client.post_database
-    posts = db.posts
-
     # login to session throigh instagram api
     print('input username')
     IGUSER = input()
@@ -45,12 +22,51 @@ def main():
     print('input password')
     PASSWD = input()
 
+    # call to api
     igapi = InstagramAPI(IGUSER,PASSWD)
-    igapi.login() # login
+
+    # start the mongo client, access the posts database
+    client = pymongo.MongoClient()
+    db = client.post_database
+    posts = db.posts
+
+    # declare & fill the queue with ObjectId types, corresponding to the values contained in the post_map
+    q = queue.Queue()
+
+    # q = fill_queue(q)
+    with open("post_map", "r") as f:
+        map_content = f.readlines()
+
+    # print(map_content)
+    for x in map_content:
+        # print('x : ', x)
+        # y = x.clone()
+        y = copy.copy(x)
+        y = y.replace("\n", "")
+        q.put(ObjectId(y))
+
+
+    # igapi.login() # login
     i=0
+
     while not q.empty():
+        igapi.login() # login
         # pprint.pprint(posts.find_one({"_id":q.get()}))
-        p = posts.find_one({"_id":q.get()})
+        entry = q.get()
+        p = posts.find_one({"_id":entry})
+
+        entry_filter = str(entry)
+        # print('map content : ', map_content)
+        # print('entry filter: ', entry_filter)
+        # entry_filter = entry_filter.replace("ObjectId('", "")
+        # entry_filter = entry_filter.replace("'", "")
+        # remove the dequeued() entry from the list
+        map_content.remove(entry_filter+"\n")
+
+        # open the post_map file, and write back the remaining entries
+        with open("post_map", "w") as f:
+            for x in map_content:
+                f.write(x)
 
         print('posting next, #'+str(i))
         print(p['image_path'])
@@ -62,10 +78,11 @@ def main():
 
 
         # sleep until next post
-        time.sleep(5)
+        # log out of session
+        igapi.logout()
+        time.sleep(20)
         i+=1
-    # log out of session
-    igapi.logout()
+
 
 def fill_queue(q):
     with open("post_map", "r") as f:
