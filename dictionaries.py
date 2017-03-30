b243376957f8edc96396794dcc89709b748e5d0c
Alexander Harris <abharris@pugetsound.edu>
2017-03-30 12:02:52 -0700

    dictionaries++

--- dictionaries.py
+++ dictionaries.py
@@ -1,6 +1,6 @@
 # get user input to choose a dictionary to ascertain content from
 def choose_dictionary():
-    print('choose dictionary, enter leading character :\n\tclay\tceramics\n\tcs\tcomputers')
+    print('choose dictionary, enter leading character :\n\tclay\tceramics\n\tcs\tcomputers\n\tups\tuniv. puget sound')
     d = input()
     if d == 'clay':
         # ceramics dict
@@ -21,17 +21,22 @@ def choose_dictionary():
             'lunaceramics' : 'https://www.instagram.com/lunaceramics/',
             'love_ceramic' : 'https://www.instagram.com/love_ceramic/',
             'lindseywherrett' : 'https://www.instagram.com/lindseywherrett/',
-            'lindseywherrett' : 'https://www.instagram.com/lindseywherrett/'
+            'nur_ceramics' : 'https://www.instagram.com/nur_ceramics/',
+            'gerhard_ceramics' : 'https://www.instagram.com/gerhard_ceramics/',
+            'kira_ni' : 'https://www.instagram.com/kira_ni/',
+            'ar.ceramics' : 'https://www.instagram.com/ar.ceramics/',
+            'notary_ceramics' : 'https://www.instagram.com/notary_ceramics/',
+            'ceramics274' : 'https://www.instagram.com/ceramics274/',
+            'ceramics.daily' : 'https://www.instagram.com/ceramics.daily/'
         }
     elif d == 'cs':
-        # dictionary of pages to acquire
+        # dictionary of cs pages to acquire
         post_dictionary = {
-            # 'natanya' : 'https://www.instagram.com/product_of_the_world',
             'mononoke' : 'https://www.instagram.com/mononoke.io/',
             'world_programmers' : 'https://www.instagram.com/worldofprogrammers/',
             'world_code' : 'https://www.instagram.com/worldcode/',
             'republic' : 'https://www.instagram.com/programmerrepublic/',
-            # 'codeness' : 'https://www.instagram.com/thecodeness/',
+            'codeness' : 'https://www.instagram.com/thecodeness/',
             'quotes' : 'https://www.instagram.com/codingquotes/',
             'buildtheweb' : 'https://www.instagram.com/buildtheweb/',
             'studiokivi' : 'https://www.instagram.com/studiokivi/',
@@ -45,11 +50,39 @@ def choose_dictionary():
             'becreatives' : 'https://www.instagram.com/becreatives/',
             'macintosh_setups' : 'https://www.instagram.com/macintosh_setups/',
             'codinblog' : 'https://www.instagram.com/codinblog/',
-            # 'thavy' : 'https://www.instagram.com/thavytillest/',
             'themaxsandelin' : 'https://www.instagram.com/themaxsandelin/',
             'codingcouple' : 'https://www.instagram.com/codingcouple/',
             'madeawkward' : 'https://www.instagram.com/madeawkward/',
             'pixelsdigital' : 'https://www.instagram.com/pixelsdigital/',
             'setuptour_' : 'https://www.instagram.com/setuptour_/'
+        }
+    elif d == 'ups':
+        # dictionary of ups pages to acquire
+        post_dictionary = {
+            'theyellowhouse_ups' : 'https://www.instagram.com/theyellowhouse_ups/',
+            'wetlands' : 'https://www.instagram.com/wetlands_magazine/',
+            'dogsofpugetsound' : 'https://www.instagram.com/dogsofpugetsound/',
+            'rmulhausen' : 'https://www.instagram.com/rmulhausen/',
+            'psloggers' : 'https://www.instagram.com/psloggers/',
+            'logger_swimming_fan_page' : 'https://www.instagram.com/logger_swimming_fan_page/',
+            'kupsthesound' : 'https://www.instagram.com/kupsthesound/',
+            'pugetsoundoutdoors' : 'https://www.instagram.com/pugetsoundoutdoors/',
+            'univpugetsound' : 'https://www.instagram.com/univpugetsound/',
+            'pugetsoundpiphi' : 'https://www.instagram.com/pugetsoundpiphi/',
+            'pugetsound_phideltatheta' : 'https://www.instagram.com/pugetsound_phideltatheta/',
+            'alphaphi_gammazeta' : 'https://www.instagram.com/alphaphi_gammazeta/',
+            'pugetsoundrsa' : 'https://www.instagram.com/pugetsoundrsa/',
+            'ups_diner' : 'https://www.instagram.com/ups_diner/',
+            'blp' : 'https://www.instagram.com/protectthebrand/',
+            'pugetsoundmusic' : 'https://www.instagram.com/pugetsoundmusic/',
+            'loggerlax' : 'https://www.instagram.com/loggerlax/',
+            'saepugetsound' : 'https://www.instagram.com/saepugetsound/',
+            'pugetsound.cwlt' : 'https://www.instagram.com/pugetsound.cwlt/',
+            'sigmachipugetsound' : 'https://www.instagram.com/sigmachipugetsound/',
+            'pugetsoundadmission' : 'https://www.instagram.com/pugetsoundadmission/',
+            'upslymcrew' : 'https://www.instagram.com/upslymcrew/',
+            'oppcafe' : 'https://www.instagram.com/oppcafe/',
+            'diversionscafe' : 'https://www.instagram.com/diversionscafe/',
+            'upscellar' : 'https://www.instagram.com/upscellar/'
         }
     return post_dictionary
