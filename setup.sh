77391d686d3e1bdc42c7bdf164e5aa20a9184770
alexander-io <abharris@pugetsound.edu>
2017-08-10 13:12:56 -0700

    ++setup script, install ig api dependencies

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ git clone https://github.com/LevPasha/Instagram-API-python.git
 sudo pip3 install pymongo
 sudo pip3 install requests_toolbelt
 sudo pip3 install moviepy
+cd Instagram-API-python && sudo python3 setup.py install && cd ..
