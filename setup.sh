a987150462c5cfb0370cd661bba6bcf9bbf09f7b
alexander-io <abharris@pugetsound.edu>
2017-08-10 15:36:18 -0700

    ++setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
-sudo apt install make
+sudo apt install make -y
+sudo apt install python3-pip -y
 mkdir posts/
 touch post_map
 git clone https://github.com/LevPasha/Instagram-API-python.git
 sudo pip3 install pymongo
 sudo pip3 install requests_toolbelt
 sudo pip3 install moviepy
-cd Instagram-API-python && sudo python3 setup.py install && cd ..
+cd Instagram-API-python && pip install -r requirements.txt && cd ..
